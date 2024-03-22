5551712c03b834357c5db5c92165c99c27b14a8a
// Sudhanshu <keysatmyfingertips@yahoo.com>
2024-03-22 10:36:17 +0000

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// Execution done
+		System.out.println("\nExecution completed successfully!");
+	}
+}
