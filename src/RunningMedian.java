36bc7a50a08a6958126a8559dc2fcda925c393a2
// Sudhanshu <keysatmyfingertips@yahoo.com>
2024-03-22 19:57:01 +0000

    Step simplification

--- src/RunningMedian.java
+++ src/RunningMedian.java
@@ -3,8 +3,8 @@ import datastructures.*;
 
 public class RunningMedian {
 
-	private static PriorityQueue<Float> minHeap;
-	private static PriorityQueue<Float> maxHeap;
+	private static PriorityQueue<Float> minPQ;
+	private static PriorityQueue<Float> maxPQ;
 	
 	final public static int HEAPPQ = 0;
 	final public static int UNSORTEDPQ = 1;
@@ -17,24 +17,16 @@ public class RunningMedian {
 		float median = 0;
 		for(int i = 0; i < numbersList.length; ++i) {
 			if(numbersList[i] < median) {
-				RunningMedian.maxHeap.offer(numbersList[i]);
-				
-				int minHeapSize = RunningMedian.minHeap.size();
-				int maxHeapSize = RunningMedian.maxHeap.size();
-				
-				if ((maxHeapSize - minHeapSize) > 1) {
-					RunningMedian.minHeap.offer(RunningMedian.maxHeap.poll());
-				}
+				RunningMedian.applyToLeftQueue(
+						numbersList[i], 
+						RunningMedian.maxPQ, 
+						RunningMedian.minPQ);
 				
 			} else {
-				RunningMedian.minHeap.offer(numbersList[i]);
-				
-				int minHeapSize = RunningMedian.minHeap.size();
-				int maxHeapSize = RunningMedian.maxHeap.size();
-				
-				if((minHeapSize - maxHeapSize) > 1) {
-					RunningMedian.maxHeap.offer(RunningMedian.minHeap.poll());
-				}
+				RunningMedian.applyToLeftQueue(
+						numbersList[i], 
+						RunningMedian.minPQ, 
+						RunningMedian.maxPQ);
 			}
 			
 			
@@ -46,33 +38,47 @@ public class RunningMedian {
 		return median;
 	}
 	
+	private static void applyToLeftQueue(
+			float number, 
+			PriorityQueue<Float> leftQueue,
+			PriorityQueue<Float> rightQueue) {
+		leftQueue.offer(number);
+		
+		int leftHeapSize = leftQueue.size();
+		int rightHeapSize = rightQueue.size();
+		
+		if ((leftHeapSize - rightHeapSize) > 1) {
+			rightQueue.offer(leftQueue.poll());
+		}
+	}
+	
 	private static void initialiseQueue(int queueNumber) {
 		switch(queueNumber) {
 		case RunningMedian.HEAPPQ:
-			RunningMedian.minHeap = new HeapPriorityQueue<Float>();
-			RunningMedian.maxHeap = new HeapPriorityQueue<Float>(Comparator.reverseOrder());
+			RunningMedian.minPQ = new HeapPriorityQueue<Float>();
+			RunningMedian.maxPQ = new HeapPriorityQueue<Float>(Comparator.reverseOrder());
 			break;
 		case RunningMedian.UNSORTEDPQ:
-			RunningMedian.minHeap = new UnsortedPriorityQueue<Float>();
-			RunningMedian.maxHeap = new UnsortedPriorityQueue<Float>(Comparator.reverseOrder());
+			RunningMedian.minPQ = new UnsortedPriorityQueue<Float>();
+			RunningMedian.maxPQ = new UnsortedPriorityQueue<Float>(Comparator.reverseOrder());
 			break;
 		case RunningMedian.SORTEDPQ:
-			RunningMedian.minHeap = new SortedPriorityQueue<Float>();
-			RunningMedian.maxHeap = new SortedPriorityQueue<Float>(Comparator.reverseOrder());
+			RunningMedian.minPQ = new SortedPriorityQueue<Float>();
+			RunningMedian.maxPQ = new SortedPriorityQueue<Float>(Comparator.reverseOrder());
 			break;
 		}
 	}
 	
 	private static float getMedian() {
-		int minHeapSize = RunningMedian.minHeap.size();
-		int maxHeapSize = RunningMedian.maxHeap.size();
+		int minHeapSize = RunningMedian.minPQ.size();
+		int maxHeapSize = RunningMedian.maxPQ.size();
 		if(minHeapSize == maxHeapSize) {
-			return (RunningMedian.minHeap.peek() + 
-					RunningMedian.maxHeap.peek()) / 2; 
+			return (RunningMedian.minPQ.peek() + 
+					RunningMedian.maxPQ.peek()) / 2; 
 		} else if (minHeapSize > maxHeapSize) {
-			return RunningMedian.minHeap.peek();
+			return RunningMedian.minPQ.peek();
 		}
 		
-		return RunningMedian.maxHeap.peek();
+		return RunningMedian.maxPQ.peek();
 	}
 }
